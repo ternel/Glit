66625945d8d48af316c6b08b6715f42bbcd885a4
William POTTIER <william.pottier@allprogrammic.com>
2012-04-03 00:33:05 +0200

    gitolite management

--- src/Glit/GitoliteBundle/Admin/Gitolite.php
+++ src/Glit/GitoliteBundle/Admin/Gitolite.php
@@ -1,14 +1,20 @@
 <?php
 namespace Glit\GitoliteBundle\Admin;
 
-use Symfony\Component\Process\Process;
 use Symfony\Component\HttpKernel\Log\LoggerInterface;
+use Glit\GitoliteBundle\Git\Repository;
 
 class Gitolite {
 
     private $logger;
     private $adminUri = 'git@localhost:gitolite-admin.git';
     private $localDir = '/tmp/glit-gitolite-admin/';
+
+    /** @var Repository */
+    private $gitRepository;
+
+    private $sshKeys;
+    private $repositories;
 
     public function __construct(LoggerInterface $logger) {
         $this->logger = $logger;
@@ -17,9 +23,78 @@ class Gitolite {
 
     protected function initialize() {
         if (!file_exists($this->localDir)) {
-            $this->execProcess(sprintf('sudo -u glit -H sh -c "mkdir %s && chmod -R 700 %s"', $this->localDir, $this->localDir));
-            $this->execProcess(sprintf('sudo -H -u glit git clone %s %s', $this->adminUri, $this->localDir));
+            $this->gitRepository = Repository::cloneRepository($this->logger, $this->adminUri, $this->localDir);
         }
+        else {
+            $this->gitRepository = new Repository($this->logger, $this->localDir);
+        }
+
+        // Load keys and repository
+        $this->loadUserKeyList();
+        $this->loadRepositories();
+
+        print_r($this->repositories);
+        $this->writeRepositoriesConf();
+    }
+
+    private function loadUserKeyList() {
+        $sourceKeys = $this->gitRepository->listFiles('keydir' . DS);
+
+        foreach ($sourceKeys as $key) {
+            list($keyName, $extension) = explode('.', $key);
+            $this->sshKeys[$keyName] = $key;
+        }
+    }
+
+    private function loadRepositories() {
+        $lines = explode("\n", $this->gitRepository->readFile('conf' . DS . 'gitolite.conf'));
+
+        $tempRepository = null;
+
+        foreach ($lines as $line) {
+            $temp = array_merge(array_filter(explode(" ", $line), 'strlen'));
+
+            if (count($temp) <= 0)
+                continue;
+
+            if ($temp[0] == 'repo') {
+                if (null !== $tempRepository) {
+                    $this->repositories[$tempRepository['name']] = $tempRepository;
+                }
+
+                $tempRepository = array(
+                    'name' => $temp[1],
+                    'users' => array()
+                );
+            }
+            else {
+                $tempRepository['users'][$temp[2]] = $temp[0];
+            }
+        }
+
+        $this->repositories[$tempRepository['name']] = $tempRepository;
+    }
+
+    private function writeRepositoriesConf($commitMessage = 'Save gitolite repositories confifuration.') {
+        $conf = '';
+
+        foreach ($this->repositories as $repository) {
+            // Add line break on all lines except first one
+            if (!empty($conf)) {
+                $conf .= "\n";
+            }
+
+            $conf .= sprintf("repo    %s\n", $repository['name']);
+
+            foreach ($repository['users'] as $name => $right) {
+                $conf .= sprintf("        %s = %s\n", $right, $name);
+            }
+        }
+
+        $confFile = 'keygen' . DS . 'gitolite.conf';
+        $this->gitRepository->saveFile($confFile, $conf);
+        $this->gitRepository->commitFile($confFile, $commitMessage);
+        $this->gitRepository->push();
     }
 
     /**
@@ -28,38 +103,90 @@ class Gitolite {
      * @param $sshKey
      */
     public function saveUserKey($name, $sshKey) {
-        $file = $this->localDir . 'keydir' . DS . $name . '.pub';
-        $this->execProcess(sprintf('sudo -u glit -H sh -c \'echo "%s" > %s\'', $sshKey, $file));
-        $this->commitFile($file, 'define ssh key named ' . $name);
-    }
+        $file = 'keydir' . DS . $name . '.pub';
 
-    public function deleteUserKey($name) {
-        $file = $this->localDir . 'keydir' . DS . $name . '.pub';
-        $this->execProcess(sprintf('sudo -H -u glit rm %s', $file));
-        $this->commitFile($file, 'remove ssh key named ' . $name);
-    }
+        $this->gitRepository->saveFile($file, $sshKey);
+        $this->sshKeys[$name] = $name . '.pub';
 
-    private function commitFile($file, $reason) {
-        $this->execProcess(sprintf('sudo -u glit -H sh -c \'cd %s; git add %s\'', $this->localDir, $file));
-        $this->execProcess(sprintf('sudo -u glit -H sh -c \'cd %s; git commit -m "%s"\'', $this->localDir, $reason));
-        $this->execProcess(sprintf('sudo -u glit -H sh -c \'cd %s; git push\'', $this->localDir));
+        $this->gitRepository->commitFile($file, 'define ssh key named ' . $name);
+        $this->gitRepository->push();
     }
 
     /**
-     * @param $command
-     * @return \Symfony\Component\Process\Process
-     * @throws RuntimeException
+     * @param $name
      */
-    protected function execProcess($command) {
-        $this->logger->addDebug('Execute : ' . $command);
-        $process = new Process($command);
-        $process->setTimeout(3600);
-        $process->run();
-        if (!$process->isSuccessful()) {
-            throw new \RuntimeException($process->getErrorOutput());
-        }
-        $this->logger->addDebug('Execute : ' . $process->getOutput());
+    public function deleteUserKey($name) {
+        $file = 'keydir' . DS . $name . '.pub';
 
-        return $process;
+        unset ($this->sshKeys[$name]);
+        $this->gitRepository->deleteFile($file);
+
+        $this->gitRepository->commitFile($file, 'remove ssh key named ' . $name);
+        $this->gitRepository->push();
     }
+
+    public function createRepository($repositoryName, $owner) {
+        if (!is_array($owner)) {
+            $owner = array($owner);
+        }
+
+        $repository = array(
+            'name' => $repositoryName,
+            'users' => array(),
+        );
+
+        foreach ($owner as $keyName) {
+            $repository['users'][$keyName] = 'RW+';
+        }
+
+        $this->repositories[$repositoryName] = $repository;
+
+        $this->writeRepositoriesConf('Create repository ' . $repositoryName);
+    }
+
+    public function removeRepository($repositoryName) {
+
+        unset($this->repositories[$repositoryName]);
+
+        $this->writeRepositoriesConf('Delete repository ' . $repositoryName);
+    }
+
+    public function addUserToRepository($repositoryName, $userKey, $write, $read) {
+        if (!is_array($userKey)) {
+            $userKey = array($userKey);
+        }
+
+        $rights = '';
+        if ($read) {
+            $rights .= 'R';
+        }
+        if ($write) {
+            $rights .= 'W';
+        }
+
+        foreach ($userKey as $keyName) {
+            $this->repositories[$repositoryName]['users'][$keyName] = $rights;
+        }
+
+        $this->writeRepositoriesConf(sprintf(
+            'Add user(s) (%s) to repository %s',
+            implode(', ', $userKey),
+            $repositoryName));
+    }
+
+    public function removeUserToRepository($repositoryName, $userKey) {
+        if (!is_array($userKey)) {
+            $userKey = array($userKey);
+        }
+
+        foreach ($userKey as $keyName) {
+            unset($this->repositories[$repositoryName]['users'][$keyName]);
+        }
+
+        $this->writeRepositoriesConf(sprintf(
+            'Remove user(s) (%s) from repository %s',
+            implode(', ', $userKey),
+            $repositoryName));
+    }
+
 }
---
+++ src/Glit/GitoliteBundle/Git/Repository.php
@@ -0,0 +1,118 @@
+<?php
+namespace Glit\GitoliteBundle\Git;
+
+use \Symfony\Component\HttpKernel\Log\LoggerInterface;
+use Symfony\Component\Process\Process;
+
+class Repository {
+
+    private $path;
+    private $logger;
+
+    public static function cloneRepository(LoggerInterface $logger, $remote, $localPath) {
+        self::staticExecProcessAsGlit($logger, $localPath, sprintf('git clone %s %s', $remote, $localPath));
+        return new self($logger, $localPath);
+    }
+
+    public function __construct(LoggerInterface $logger, $path) {
+        $this->logger = $logger;
+        $this->path = $path;
+    }
+
+    /**
+     * Commit some files
+     * @param $file string|array Files to commit
+     * @param $message string Commit message
+     */
+    public function commitFile($file, $message) {
+        if (!is_array($file)) {
+            $file = array($file);
+        }
+
+        foreach ($file as $f) {
+            $this->execProcessAsGlit('git add ', $f);
+        }
+
+        $this->execProcessAsGlit(sprintf('git commit -m "%s"', $message));
+
+    }
+
+    /**
+     * Push repository
+     */
+    public function push() {
+        $this->execProcessAsGlit('git push');
+    }
+
+    /**
+     * Push repository
+     */
+    public function pull() {
+        $this->execProcessAsGlit('git pull');
+    }
+
+    // File Manipulation
+    // ---------------------------------
+
+    /**
+     * @param $file
+     * @param $data
+     */
+    public function saveFile($file, $data) {
+        $this->execProcessAsGlit(sprintf('echo "%s" > %s', $data, $file));
+    }
+
+    /**
+     * @param $file
+     */
+    public function deleteFile($file) {
+        $this->execProcessAsGlit('rm %s' . $file);
+    }
+
+    /**
+     * @param $file
+     * @return string
+     */
+    public function readFile($file) {
+        return $this->execProcessAsGlit('cat ' . $file)->getOutput();
+    }
+
+    public function listFiles($directory) {
+        return array_merge(array_filter(
+            explode("\n", $this->execProcessAsGlit(sprintf('ls %s | cat', $directory))->getOutput()),
+            'strlen'
+        ));
+    }
+
+    // Process Execution
+    // ---------------------------------
+
+    /**
+     * @param $command
+     * @return \Symfony\Component\Process\Process
+     * @throws RuntimeException
+     */
+    protected function execProcessAsGlit($command) {
+        return self::staticExecProcessAsGlit($this->logger, $this->path, $command);
+    }
+
+    /**
+     * @param $command
+     * @return \Symfony\Component\Process\Process
+     * @throws RuntimeException
+     */
+    protected static function staticExecProcessAsGlit(LoggerInterface $logger, $path, $command) {
+        $command = sprintf('sudo -u glit -H sh -c \'cd %s; %s\'', $path, $command);
+
+        $logger->addDebug('Execute : ' . $command);
+        $process = new Process($command);
+        $process->setTimeout(3600);
+        $process->run();
+        if (!$process->isSuccessful()) {
+            throw new \RuntimeException($process->getErrorOutput());
+        }
+        $logger->addDebug('Result : ' . $process->getOutput());
+
+        return $process;
+    }
+}
