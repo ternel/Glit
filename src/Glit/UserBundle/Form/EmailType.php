dfd78dbd618a553532d22c8712c8132e73391f17
William POTTIER <william.pottier@allprogrammic.com>
2012-04-07 00:01:16 +0200

    Add emails view in user profile

---
+++ src/Glit/UserBundle/Form/EmailType.php
@@ -0,0 +1,23 @@
+<?php
+namespace Glit\UserBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilder;
+use Symfony\Component\Validator\Constraints as Constraints;
+
+class ProfileType extends AbstractType {
+
+    public function buildForm(FormBuilder $builder, array $options) {
+        $builder->add('address', 'email');
+    }
+
+    public function getDefaultOptions(array $options) {
+        return array(
+            'data_class' => 'Glit\UserBundle\Entity\Email',
+        );
+    }
+
+    public function getName() {
+        return 'user_email';
+    }
+}
